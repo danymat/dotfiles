47e2e57c042f207c6ec4bb545a0cd62b80098c10
Daniel Mathiot <d.danymat@gmail.com>
2021-08-13 15:45:03 +0200

    Update

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,106 +4,18 @@ vim.cmd [[
 ]]
 
 require('configuration')
+--require('configs.lsp')
 require('plugins')
 require('keybinds')
 
 vim.cmd [[
-let g:airline_theme='deus'
-
-lua << EOF
-require'telescope'.setup { 
-    defaults = { winblend = 10 } ,
-    extensions = {
-        frecency = {
-            show_scores = true,
-            ignore_patterns = {"*.git/*", "*/tmp/*"},
-            }
-        }
-}
-require'telescope'.load_extension'frecency'
-require'telescope'.load_extension'project'
-EOF
-
-let g:goyo_width = 75
-
-
-" -------------
-" NERDTree (NT)
-" -------------
-" Exit Vim if NERDTree is the only window left.
-"autocmd BufEnter * if tabpagenr('$') == 1 && winnr('$') == 1 && exists('b:NERDTree') && b:NERDTree.isTabTree() |
-"    \ quit | endif
-
-" If another buffer tries to replace NERDTree, put it in the other window, and bring back NERDTree.
-autocmd BufEnter * if bufname('#') =~ 'NERD_tree_\d\+' && bufname('%') !~ 'NERD_tree_\d\+' && winnr('$') > 1 |
-            \ let buf=bufnr() | buffer# | execute "normal! \<C-W>w" | execute 'buffer'.buf | endif
-
-" ---------
-" LSP Stuff (completion.nvim and lspconfig) (LSP)
-" ---------
-
-lua require('configs.lsp')
-
 " Use <Tab> and <S-Tab> to navigate through popup menu
 inoremap <expr> <Tab>   pumvisible() ? "\<C-n>" : "\<Tab>"
 inoremap <expr> <S-Tab> pumvisible() ? "\<C-p>" : "\<S-Tab>"
 
-lua << EOF
-require'compe'.setup {
-enabled = true;
-autocomplete = true;
-debug = false;
-min_length = 1;
-preselect = 'enable';
-throttle_time = 80;
-source_timeout = 200;
-incomplete_delay = 400;
-max_abbr_width = 100;
-max_kind_width = 100;
-max_menu_width = 100;
-documentation = true;
-
-source = {
-    path = true;
-    buffer = true;
-    calc = true;
-    nvim_lsp = true;
-    nvim_lua = true;
-    vsnip = true;
-    neorg = true;
-    };
-}
-EOF
 inoremap <silent><expr> <CR>      compe#confirm('<CR>')
 inoremap <silent><expr> <C-Space> compe#complete()
 
-
-lua <<EOF
-local parser_configs = require('nvim-treesitter.parsers').get_parser_configs()
-
-parser_configs.norg = {
-    install_info = {
-        url = "https://github.com/vhyrro/tree-sitter-norg",
-        files = { "src/parser.c" },
-        branch = "main"
-    },
-}
-
-require'nvim-treesitter.configs'.setup {
-ensure_installd = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-highlight = { enable = true },
-indent = { enable = true, },
-incremental_selection = { 
-enable = true,
-keymaps = {
-    init_selection = "<CR>",
-    scope_incremental = "<CR>",
-    node_incremental = "<TAB>",
-    node_decremental = "<S-TAB>",
-    },
-},
-}
-EOF
 
 " ---------
 " GitGutter
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -7,8 +7,13 @@ packer.startup(function(use)
     use { 'tpope/vim-surround' }
     use { 'rakr/vim-one' }
     use { 'joshdick/onedark.vim' }
-    use { 'vim-airline/vim-airline' }
-    use { 'vim-airline/vim-airline-themes' }
+    use { 
+        'vim-airline/vim-airline',
+        config = function()
+            vim.g.airline_theme = 'deus'
+        end,
+        requires = "vim-airline/vim-airline-themes"
+    }
     use { 'tpope/vim-fugitive' }
     use { 'preservim/nerdcommenter' }
     use { 'kyazdani42/nvim-web-devicons' }
@@ -17,22 +22,105 @@ packer.startup(function(use)
     use { 'airblade/vim-gitgutter' }
     use { 'nvim-lua/popup.nvim' }
     use { 'nvim-lua/plenary.nvim' }
-    use { 'nvim-telescope/telescope.nvim' }
+
+    use { 
+        'nvim-telescope/telescope.nvim',
+        config = function()
+            require'telescope'.setup { 
+                defaults = { winblend = 10 } ,
+                extensions = {
+                    frecency = {
+                        show_scores = true,
+                        ignore_patterns = {"*.git/*", "*/tmp/*"},
+                    }
+                }
+            }
+            require'telescope'.load_extension'frecency'
+            require'telescope'.load_extension'project'
+        end
+    }
+
     use { 'ayu-theme/ayu-vim' }
     use { 'junegunn/goyo.vim' }
-    use { 'nvim-treesitter/nvim-treesitter' }
-    use { 'nvim-treesitter/playground' }
-    use { 'neovim/nvim-lspconfig' }
-    use { 'hrsh7th/nvim-compe' }
+    use { 
+        'nvim-treesitter/nvim-treesitter',
+        run = 'TSUpdate',
+        config = function ()
+            local parser_configs = require('nvim-treesitter.parsers').get_parser_configs()
+
+            parser_configs.norg = {
+                install_info = {
+                    url = "https://github.com/vhyrro/tree-sitter-norg",
+                    files = { "src/parser.c" },
+                    branch = "main"
+                },
+            }
+
+            require'nvim-treesitter.configs'.setup {
+                ensure_installd = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+                highlight = { enable = true },
+                indent = { enable = true, },
+                incremental_selection = { 
+                    enable = true,
+                    keymaps = {
+                        init_selection = "<CR>",
+                        scope_incremental = "<CR>",
+                        node_incremental = "<TAB>",
+                        node_decremental = "<S-TAB>",
+                    },
+                },
+            }
+        end
+    }
+
+    use { 
+        'hrsh7th/nvim-compe',
+        config = function ()
+            require'compe'.setup {
+                enabled = true;
+                autocomplete = true;
+                debug = false;
+                min_length = 1;
+                preselect = 'enable';
+                throttle_time = 80;
+                source_timeout = 200;
+                incomplete_delay = 400;
+                max_abbr_width = 100;
+                max_kind_width = 100;
+                max_menu_width = 100;
+                documentation = true;
+
+                source = {
+                    path = true;
+                    buffer = true;
+                    calc = true;
+                    nvim_lsp = true;
+                    nvim_lua = true;
+                    vsnip = true;
+                    neorg = true;
+                };
+            }
+        end
+    }
+
     use { 'szw/vim-maximizer' }
-    use { 'ThePrimeagen/harpoon' }
-    use { 'kabouzeid/nvim-lspinstall' }
+
+    use { 
+        'kabouzeid/nvim-lspinstall',
+        config = function ()
+           require('configs.lsp') 
+        end,
+        requires = {
+            "neovim/nvim-lspconfig",
+            "ray-x/lsp_signature.nvim"
+            }
+        }
+
     use { 'Yggdroot/indentLine' }
     use { 'jiangmiao/auto-pairs' }
     use { 'sbdchd/neoformat' }
     use { 'hrsh7th/vim-vsnip' }
     use { 'nvim-telescope/telescope-project.nvim' }
-    use { 'ray-x/lsp_signature.nvim' }
     use { 'nvim-treesitter/nvim-tree-docs' }
     use { 'nacro90/numb.nvim' }
     use { 'danymat/neorg' }
