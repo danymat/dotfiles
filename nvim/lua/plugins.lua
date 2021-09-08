f8e53f472da3b29892b9f3f0d2fc38317ae49b90
Danymat <5306901+danymat@users.noreply.github.com>
2021-09-08 13:27:55 +0200

    Update

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -178,11 +178,11 @@ packer.startup(function(use)
             require('nvim-autopairs').setup{
                 fast_wrap = { map = '€'},
             }
-            --require("nvim-autopairs.completion.cmp").setup({
-                --map_cr = true, --  map <CR> on insert mode
-                --map_complete = true, -- it will auto insert `(` after select function or method item
-                --auto_select = true -- automatically select the first item
-            --})
+            require("nvim-autopairs.completion.cmp").setup({
+                map_cr = true, --  map <CR> on insert mode
+                map_complete = true, -- it will auto insert `(` after select function or method item
+                auto_select = true -- automatically select the first item
+            })
         end,
         after = "nvim-cmp"
     }
