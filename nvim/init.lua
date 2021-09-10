8b1d32b86ba55291b79f5771d4d72dbb9401ab48
Danymat <5306901+danymat@users.noreply.github.com>
2021-09-10 17:03:45 +0200

    Update

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,29 +1,4 @@
---vim.cmd [[
-    --colorscheme ayu " other good themes are dracula and monokai
-    --let ayucolor="mirage"
---]]
---
-require('rose-pine').set()
-vim.g.rose_pine_variant = 'moon'
-vim.g.rose_pine_enable_italics = true
-vim.g.rose_pine_disable_background = false
-
-
 require('globals')
 require('configuration')
 require('plugins')
 require('keybinds')
-
-vim.cmd [[
-" Use <Tab> and <S-Tab> to navigate through popup menu
-inoremap <expr> <Tab>   pumvisible() ? "\<C-n>" : "\<Tab>"
-inoremap <expr> <S-Tab> pumvisible() ? "\<C-p>" : "\<S-Tab>"
-
-" ---------
-" GitGutter
-" Disable all concurrent mappings of <leader>h...
-" ---------
-let g:gitgutter_map_keys = 0
-
-
-]]
