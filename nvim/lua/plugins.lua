30f18572380df8c762e7d919c83f6d954db0c2ae
Danymat <5306901+danymat@users.noreply.github.com>
2021-10-15 21:53:31 +0200

    Update

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -307,7 +307,7 @@ packer.startup({
 				"hrsh7th/cmp-path",
 				"saadparwaiz1/cmp_luasnip",
 				"L3MON4D3/LuaSnip",
-                "onsails/lspkind-nvim"
+				"onsails/lspkind-nvim",
 			},
 			after = "neogen",
 		})
