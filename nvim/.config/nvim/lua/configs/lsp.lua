e3bf27611dbc1583765811b90bb098a719d82e69
Danymat <5306901+danymat@users.noreply.github.com>
2021-11-27 13:26:07 +0100

    Update

--- nvim/.config/nvim/lua/configs/lsp.lua
+++ nvim/.config/nvim/lua/configs/lsp.lua
@@ -27,7 +27,7 @@ end
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
 
--- See https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md for more lsp servers
+-- See https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md for more lsp servers
 local configs = require("lspconfig/configs")
 
 -- Personal lsp
