5289b642e79d8bc31fc396f7303e5b812e6dcf8f
Danymat <5306901+danymat@users.noreply.github.com>
2021-12-03 23:53:21 +0100

    Update

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -255,13 +255,14 @@ packer.startup({
 		-- })
 
 		use({
-			"~/Developer/neovim",
+			"rose-pine/neovim",
 			as = "rose-pine",
 			config = function()
 				vim.g.rose_pine_variant = "moon"
 				vim.g.rose_pine_bold_vertical_split_line = true
-				vim.g.rose_pine_enable_italics = true
+                vim.g.rose_pine_disable_italics = false
 				vim.g.rose_pine_disable_background = false
+                vim.g.rose_pine_disable_float_background = true
 				vim.cmd([[ colorscheme rose-pine ]])
 			end,
 		})
