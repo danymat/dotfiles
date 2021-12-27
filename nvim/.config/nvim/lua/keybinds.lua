222b6ab1ea7e823b15ef6a7940584dc3fb40a0b5
danymat <d.danymat@gmail.com>
2021-12-27 11:27:18 +0100

    Update

--- nvim/.config/nvim/lua/keybinds.lua
+++ nvim/.config/nvim/lua/keybinds.lua
@@ -138,32 +138,6 @@ vim.api.nvim_set_keymap(
 	{ noremap = true, silent = false }
 )
 
--- Venn.nvim
--- enable or disable keymappings for venn
-function _G.toggle_venn()
-	local venn_enabled = vim.inspect(vim.b.venn_enabled)
-	if venn_enabled == "nil" then
-		vim.b.venn_enabled = true
-		vim.cmd([[setlocal ve=all]])
-		-- draw a line on HJKL keystokes
-		vim.api.nvim_buf_set_keymap(0, "n", "J", "<C-v>j:VBox<cr>", { noremap = true })
-		vim.api.nvim_buf_set_keymap(0, "n", "K", "<C-v>k:VBox<cr>", { noremap = true })
-		vim.api.nvim_buf_set_keymap(0, "n", "L", "<C-v>l:VBox<cr>", { noremap = true })
-		vim.api.nvim_buf_set_keymap(0, "n", "H", "<C-v>h:VBox<cr>", { noremap = true })
-		-- draw a box by pressing "f" with visual selection
-		vim.api.nvim_buf_set_keymap(0, "v", "f", ":VBox<cr>", { noremap = true })
-		print("Enabled Venn")
-	else
-		vim.cmd([[setlocal ve=]])
-		vim.cmd([[mapclear <buffer>]])
-		vim.b.venn_enabled = nil
-		print("Disabled Venn")
-	end
-end
-
--- toggle keymappings for venn using <leader>v
-vim.api.nvim_set_keymap("n", "<leader>v", ":lua toggle_venn()<cr>", { noremap = true })
-
 -- Inverse of join-line
 vim.api.nvim_set_keymap("n", "<C-j>", "i<CR><Esc>J", { noremap = true })
 
