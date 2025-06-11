775aecb26c9b8f432cdabf66a5f51b1d9fd21d08
Danymat <5306901+danymat@users.noreply.github.com>
2025-06-11 13:53:23 +0200

    Update

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+fish_add_path /opt/nvim-linux-x86_64/bin
+alias v="nvim"
+
+# Created by `pipx` on 2025-06-02 13:59:38
+set PATH $PATH /home/danymat/.local/bin
+
+set --universal nvm_default_version v24.0.1
