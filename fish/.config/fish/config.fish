0b97890b512f23d3e0cb277a2e2c6d9bff4d386b
danymat <5306901+danymat@users.noreply.github.com>
2026-04-04 17:14:03 +0200

    Update

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -10,6 +10,8 @@ fish_vi_key_bindings
 alias v="nvim"
 alias vim="nvim"
 alias n="navi"
+alias ls="eza"
+alias ll="eza -lH"
 
 # Added by OrbStack: command-line tools and integration
 # This won't be added again if you remove it.
